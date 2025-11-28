dea4ba984fda7c40ee3c50b9d2eaa9d1ac1f505f
YourGithubUsername <hamadasemo0@gmail.com>
2025-11-28 23:28:09 +0200

    made the basic sudoku gui and called it in main

--- Main.java
+++ Main.java
@@ -1,16 +1,8 @@
-
+import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        int N = 9; // 9x9 board, change to 4, 16, etc, for other sizes
-        SudokuGenerator sg = new SudokuGenerator(N);
-        System.out.println("Generated Sudoku Board:");
-        sg.printBoard();
-
-        // Optionally, demonstrate puzzle generation as well:
-        sg.generatePuzzle(40, 2); // Remove 40 cells, allow up to 2 solutions
-        System.out.println("Generated Sudoku Puzzle (up to 2 solutions):");
-        sg.printBoard();
+        SwingUtilities.invokeLater(() -> new SudokuGUI());
     }
 }
 
