bbfba7b47886e4b062d9e3e623e60134521d5d35
Farouk-Ezz-ElArab <faroukezz9595@gmail.com>
2025-12-22 00:30:00 +0200

    Bug fixes

--- utils/GameFactory.java
+++ utils/GameFactory.java
@@ -5,27 +5,28 @@ import model.Game;
 
 
 public class GameFactory {
-    
-    
-    public static Game createDifficultyGame(Game sourceGame, DifficultyEnum difficulty, RandomPairs randomPairs) {
-        int[][] board = sourceGame.getBoard();
+
+    public static Game createDifficultyGame(Game source, DifficultyEnum diff, RandomPairs randomPairs) {
+        return switch (diff) {
+            case EASY -> createGame(source, 10, randomPairs);
+            case MEDIUM -> createGame(source, 20, randomPairs);
+            case HARD -> createGame(source, 25, randomPairs);
+        };
+    }
+
+    private static Game createGame(Game source, int cells, RandomPairs randomPairs) {
+        int[][] board = source.getBoard();
         int size = board.length;
-        
-        
+
         int[][] newBoard = new int[size][size];
         for (int i = 0; i < size; i++) {
             System.arraycopy(board[i], 0, newBoard[i], 0, size);
         }
-        
-        
-        java.util.List<int[]> pairs = randomPairs.generateDistinctPairs(difficulty.getCellsToRemove());
-        for (int[] pair : pairs) {
-            int row = pair[0];
-            int col = pair[1];
-            newBoard[row][col] = 0; 
+
+        for (int[] p : randomPairs.generateDistinctPairs(cells)) {
+            newBoard[p[0]][p[1]] = 0;
         }
-        
+
         return new Game(newBoard);
     }
 }
-
